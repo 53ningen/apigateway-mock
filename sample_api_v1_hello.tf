a294905bcb63ea257eb1810e27a4c5667d00f5c1
gomi_ningen <korun24276@gmail.com>
2017-03-18 04:03:18 +0900

    Add SampleAPI POST /v1/hello

--- sample_api_v1_hello.tf
+++ sample_api_v1_hello.tf
@@ -38,3 +38,43 @@ resource "aws_api_gateway_integration_response" "SampleAPI-GET-v1-hello-200" {
     "application/json" = "${file("json/SampleAPI/v1/hello/get-200.json")}"
   }
 }
+
+
+// POST
+resource "aws_api_gateway_method" "SampleAPI-POST-v1-hello" {
+  rest_api_id   = "${aws_api_gateway_rest_api.SampleAPI.id}"
+  resource_id   = "${aws_api_gateway_resource.SampleAPI-v1-hello.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "SampleAPI-POST-v1-hello" {
+  rest_api_id = "${aws_api_gateway_rest_api.SampleAPI.id}"
+  resource_id = "${aws_api_gateway_resource.SampleAPI-v1-hello.id}"
+  http_method = "${aws_api_gateway_method.SampleAPI-POST-v1-hello.http_method}"
+  type        = "MOCK"
+
+  request_templates {
+    "application/json" = <<EOF
+{"statusCode": 200}
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "SampleAPI-POST-v1-hello-200" {
+  rest_api_id = "${aws_api_gateway_rest_api.SampleAPI.id}"
+  resource_id = "${aws_api_gateway_resource.SampleAPI-v1-hello.id}"
+  http_method = "${aws_api_gateway_method.SampleAPI-POST-v1-hello.http_method}"
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "SampleAPI-POST-v1-hello-200" {
+  rest_api_id = "${aws_api_gateway_rest_api.SampleAPI.id}"
+  resource_id = "${aws_api_gateway_resource.SampleAPI-v1-hello.id}"
+  http_method = "${aws_api_gateway_method.SampleAPI-POST-v1-hello.http_method}"
+  status_code = "${aws_api_gateway_method_response.SampleAPI-POST-v1-hello-200.status_code}"
+
+  response_templates {
+    "application/json" = "${file("json/SampleAPI/v1/hello/post-200.json")}"
+  }
+}
